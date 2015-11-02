b7075cd5d74870710dfa5e3ebe8b4cb5c88a02fe
Alexander Novikov <anovikov@granpm.com>
2015-11-02 07:07:20 -0800

    Remove comma skip on channel escaping in executePam() function

--- messaging/pubnub.go
+++ messaging/pubnub.go
@@ -763,9 +763,9 @@ func (pub *Pubnub) executePam(channel string, read, write bool, ttl int,
 
 	if strings.TrimSpace(channel) != "" {
 		if isAudit {
-			channelParam = fmt.Sprintf("channel=%s", queryEscapeMultiple(channel, ","))
+			channelParam = fmt.Sprintf("channel=%s", url.QueryEscape(channel))
 		} else {
-			channelParam = fmt.Sprintf("channel=%s&", queryEscapeMultiple(channel, ","))
+			channelParam = fmt.Sprintf("channel=%s&", url.QueryEscape(channel))
 		}
 		noChannel = false
 	}
