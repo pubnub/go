46e9e7de8e65c26478db1c8fe4f862fb9928a744
Alexander Novikov <anovikov@granpm.com>
2016-02-06 03:09:44 -0800

    Rewrite time test

--- messaging/tests/pubnubTime_test.go
+++ messaging/tests/pubnubTime_test.go
@@ -3,8 +3,10 @@
 package tests
 
 import (
-	"fmt"
 	"github.com/pubnub/go/messaging"
+	"github.com/stretchr/testify/assert"
+	"strconv"
+	"strings"
 	"testing"
 )
 
@@ -19,36 +21,24 @@ func TestTimeStart(t *testing.T) {
 func TestServerTime(t *testing.T) {
 	pubnubInstance := messaging.NewPubnub(PubKey, SubKey, "", "", false, "")
 
-	returnTimeChannel := make(chan []byte)
+	assert := assert.New(t)
+	successChannel := make(chan []byte)
 	errorChannel := make(chan []byte)
-	responseChannel := make(chan string)
-	waitChannel := make(chan string)
 
-	go pubnubInstance.GetTime(returnTimeChannel, errorChannel)
-	go ParseTimeResponse(returnTimeChannel, responseChannel)
-	go ParseErrorResponse(errorChannel, responseChannel)
-	go WaitForCompletion(responseChannel, waitChannel)
-	ParseWaitResponse(waitChannel, t, "Time")
-}
+	go pubnubInstance.GetTime(successChannel, errorChannel)
+	select {
+	case value := <-successChannel:
+		response := string(value)
+		timestamp, err := strconv.Atoi(strings.Trim(response, "[]"))
+		if err != nil {
+			assert.Fail(err.Error())
+		}
 
-// ParseTimeResponse parses the time response from the pubnub api.
-// On error the test fails.
-func ParseTimeResponse(returnChannel chan []byte, responseChannel chan string) {
-	for {
-		value, ok := <-returnChannel
-		if !ok {
-			break
-		}
-		if string(value) != "[]" {
-			response := fmt.Sprintf("%s", value)
-			if response != "" {
-				responseChannel <- "Server time: passed."
-				break
-			} else {
-				responseChannel <- "Server time: failed."
-				break
-			}
-		}
+		assert.NotZero(timestamp)
+	case err := <-errorChannel:
+		assert.Fail(string(err))
+	case <-timeouts(10):
+		assert.Fail("Getting server timestamp timeout")
 	}
 }
 
