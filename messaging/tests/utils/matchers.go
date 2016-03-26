4171e486c8a5e94d0c2a28549ebbfffd39717439
Alexander Novikov <anovikov@granpm.com>
2016-03-26 01:49:22 -0700

    Add subscribe url matcher (with cg support)

--- messaging/tests/utils/matchers.go
+++ messaging/tests/utils/matchers.go
@@ -54,12 +54,8 @@ interactionsLoop:
 		}
 
 		if !m.matchPath(expectedURL.Path, r.URL.Path) {
-			// fmt.Println("!!!!!!!!!!!!!!paths doesnt match", expectedURL.Path, r.URL.Path)
 			continue
-		} else {
-			// fmt.Println("!!!!!!!!!!!!!!paths MATCH", expectedURL.Path, r.URL.Path)
 		}
-
 		eQuery := expectedURL.Query()
 		aQuery := r.URL.Query()
 
@@ -80,14 +76,40 @@ interactionsLoop:
 }
 
 func (m *PubnubMatcher) matchPath(expected, actual string) bool {
-	re := regexp.MustCompile("^(/subscribe/[^/]+/)([^/]+)(/.+)$")
+	if isSubscribeRe.MatchString(expected) && isSubscribeRe.MatchString(actual) {
+		return urlsMatch(expected, actual)
+	} else {
+		return expected == actual
+	}
+}
 
-	eAllMatches := re.FindAllStringSubmatch(expected, -4)
-	aAllMatches := re.FindAllStringSubmatch(actual, -4)
+func errorInteractionNotFound(
+	interactions []*cassette.Interaction) error {
+
+	var urlsBuffer bytes.Buffer
+
+	for _, i := range interactions {
+		urlsBuffer.WriteString(i.URL)
+		urlsBuffer.WriteString("\n")
+	}
+
+	return errors.New(fmt.Sprintf(
+		"Interaction not found in:\n%s",
+		urlsBuffer.String()))
+}
+
+var isSubscribeRe = regexp.MustCompile("^/subscribe/.*$")
+var subscribePathRe = regexp.MustCompile("^((?:http|https)://[^/]+)?(/subscribe/[^/]+/)([^/]+)(/[^?]+)?.+$")
+
+func urlsMatch(expected, actual string) bool {
+	eAllMatches := subscribePathRe.FindAllStringSubmatch(expected, -4)
+	aAllMatches := subscribePathRe.FindAllStringSubmatch(actual, -4)
+
+	fmt.Println(eAllMatches[0][1], aAllMatches[0][1])
 
 	if len(eAllMatches) > 0 && len(aAllMatches) > 0 {
-		eMatches := eAllMatches[0][1:]
-		aMatches := aAllMatches[0][1:]
+		eMatches := eAllMatches[0][2:]
+		aMatches := aAllMatches[0][2:]
 
 		if eMatches[0] != aMatches[0] {
 			return false
@@ -107,23 +129,39 @@ func (m *PubnubMatcher) matchPath(expected, actual string) bool {
 			return false
 		}
 
+		eUrl, err := url.Parse(expected)
+		if err != nil {
+			panic(err.Error)
+		}
+
+		aUrl, err := url.Parse(actual)
+		if err != nil {
+			panic(err.Error)
+		}
+
+		eQuery := eUrl.Query()
+		aQuery := aUrl.Query()
+
+		for fKey, _ := range eQuery {
+			if fKey == "channel-group" {
+				if _, ok := aQuery["channel-group"]; ok {
+					eCgs := eQuery.Get(fKey)
+					aCgs := aQuery.Get(fKey)
+					eChannels := strings.Split(eCgs, ",")
+					aChannels := strings.Split(aCgs, ",")
+					return AssertStringSliceElementsEqual(eChannels, aChannels)
+				} else {
+					return false
+				}
+			} else {
+				if aQuery[fKey] == nil || eQuery.Get(fKey) != aQuery.Get(fKey) {
+					return false
+				}
+			}
+		}
+
 		return true
 	} else {
-		return expected == actual
+		return false
 	}
-}
-
-func errorInteractionNotFound(
-	interactions []*cassette.Interaction) error {
-
-	var urlsBuffer bytes.Buffer
-
-	for _, i := range interactions {
-		urlsBuffer.WriteString(i.URL)
-		urlsBuffer.WriteString("\n")
-	}
-
-	return errors.New(fmt.Sprintf(
-		"Interaction not found in:\n%s",
-		urlsBuffer.String()))
 }
