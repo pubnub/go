cfebffd6fd16e58ed8ab5aa8cae653dadcc34818
Alexander Novikov <anovikov@granpm.com>
2016-04-11 10:37:35 -0700

    Add splitItems test

--- messaging/helpers_test.go
+++ messaging/helpers_test.go
@@ -1,8 +1,9 @@
 package messaging
 
 import (
-	"github.com/stretchr/testify/assert"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestAddPnpresToString(t *testing.T) {
@@ -12,4 +13,12 @@ func TestAddPnpresToString(t *testing.T) {
 	assert.Equal("qwer-pnpres", addPnpresToString("qwer"))
 	assert.Equal("qwer-pnpres,asdf-pnpres,zxcv-pnpres",
 		addPnpresToString("qwer,asdf,zxcv"))
+}
+
+func TestSplitItems(t *testing.T) {
+	assert := assert.New(t)
+
+	assert.Equal([]string{}, splitItems(""))
+	assert.Equal([]string{"ch1"}, splitItems("ch1"))
+	assert.Equal([]string{"ch1", "ch2"}, splitItems("ch1,ch2"))
 }
