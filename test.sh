aec8d9c7629eec0d5d46b74447339a2686e5da87
Alexander Novikov <anovikov@granpm.com>
2016-04-11 08:52:44 -0700

    Decrease coverage folders depth to 2

--- test.sh
+++ test.sh
@@ -3,7 +3,8 @@
 set -e
 echo "" > coverage.txt
 
-for d in $(find  ./* -maxdepth 10 -type d ! -ipath '*gae*'); do
+
+for d in $(find  ./* -maxdepth 2 -type d ! -ipath '*gae*'); do
     if ls $d/*_test.go &> /dev/null; then
         go test -v -coverprofile=profile.out -covermode=atomic $d
         if [ -f profile.out ]; then
