64df3a61a3be17e2d4af5da7e7c82161f8cf19a4
Alexander Novikov <anovikov@granpm.com>
2016-04-17 09:57:54 -0700

    Add go1.4 travis instructions

--- test.sh
+++ test.sh
@@ -3,6 +3,9 @@
 set -e
 echo "" > coverage.txt
 go version
+if [[ $TRAVIS_GO_VERSION == 1.4 ]]; then
+  go get golang.org/x/tools/cmd/cover
+fi
 
 go test -coverprofile=unit_tests.out -covermode=atomic -coverpkg=./messaging ./messaging/
 go test -coverprofile=functional_tests.out -covermode=atomic -coverpkg=./messaging ./messaging/tests/
