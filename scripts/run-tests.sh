f0ba029f252e60e33f0b4e9886f3ca30b7c9912e
Rajat Kalsy <crimsonred@users.noreply.github.com>
2018-06-11 16:50:45 +0530

    enable helpers and utils tests in travis

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -3,11 +3,15 @@
 set -e
 echo "" > coverage.txt
 
-go test -coverprofile=functional_tests.out -covermode=atomic -coverpkg=./ ./
+go test -v -coverprofile=functional_tests.out -covermode=atomic -coverpkg=./ ./
+
+go test -v -coverprofile=utils_tests.out -covermode=atomic -coverpkg=./ ./utils/
+
+go test -v -coverprofile=helpers_tests.out -covermode=atomic -coverpkg=./ ./tests/helpers/
 
 go test -v -coverprofile=integration_tests.out \
 -covermode=atomic -coverpkg=./ ./tests/e2e/
 
-gocovmerge functional_tests.out integration_tests.out > coverage.txt
+gocovmerge functional_tests.out integration_tests.out utils_tests.out helpers_tests.out > coverage.txt
 
-rm integration_tests.out functional_tests.out
+rm integration_tests.out functional_tests.out utils_tests.out helpers_tests.out
