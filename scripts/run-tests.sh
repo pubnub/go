e706e2506143ec8ac77da12387621d7777b91c8b
Rajat Kalsy <crimsonred@users.noreply.github.com>
2018-06-15 19:34:38 +0530

    added -race flag

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -3,13 +3,13 @@
 set -e
 echo "" > coverage.txt
 
-go test -v -coverprofile=functional_tests.out -covermode=atomic -coverpkg=./ ./
+go test -v -race -coverprofile=functional_tests.out -covermode=atomic -coverpkg=./ ./
 
-go test -v -coverprofile=utils_tests.out -covermode=atomic -coverpkg=./ ./utils/
+go test -v -race -coverprofile=utils_tests.out -covermode=atomic -coverpkg=./ ./utils/
 
-go test -v -coverprofile=helpers_tests.out -covermode=atomic -coverpkg=./ ./tests/helpers/
+go test -v -race -coverprofile=helpers_tests.out -covermode=atomic -coverpkg=./ ./tests/helpers/
 
-go test -v -coverprofile=integration_tests.out \
+go test -v -race -coverprofile=integration_tests.out \
 -covermode=atomic -coverpkg=./ ./tests/e2e/
 
 gocovmerge functional_tests.out integration_tests.out utils_tests.out helpers_tests.out > coverage.txt
