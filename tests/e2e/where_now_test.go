dc4b67f08e94df99472893e17880254d35d2cad7
Rajat Kalsy <crimsonred@users.noreply.github.com>
2018-07-13 17:41:41 +0530

    coverage improvements

--- tests/e2e/where_now_test.go
+++ tests/e2e/where_now_test.go
@@ -47,6 +47,33 @@ func TestWhereNowMultipleChannels(t *testing.T) {
 	assert.Equal("b", res.Channels[1])
 }
 
+func TestWhereNowMultipleChannels(t *testing.T) {
+	assert := assert.New(t)
+	interceptor := stubs.NewInterceptor()
+	interceptor.AddStub(&stubs.Stub{
+		Method:             "GET",
+		Path:               "/v2/presence/sub-key/sub-c-e41d50d4-43ce-11e8-a433-9e6b275e7b64/uuid/person-uuid",
+		Query:              "",
+		ResponseBody:       "{\"status\": 200, \"message\": \"OK\", \"payload\": {\"channels\": [\"a\",\"b\"]}, \"service\": \"Presence\"}",
+		IgnoreQueryKeys:    []string{"pnsdk", "uuid"},
+		ResponseStatusCode: 200,
+	})
+
+	pn := pubnub.NewPubNub(config)
+	pn.Config.UseHTTP2 = true
+	pn.SetClient(interceptor.GetClient())
+
+	res, _, err := pn.WhereNow().
+		UUID("person-uuid").
+		Execute()
+
+	assert.Nil(err)
+
+	assert.Equal(2, len(res.Channels))
+	assert.Equal("a", res.Channels[0])
+	assert.Equal("b", res.Channels[1])
+}
+
 func TestWhereNowNoUUID(t *testing.T) {
 	assert := assert.New(t)
 
