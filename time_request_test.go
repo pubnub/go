b731957485d0762042bb0c912001db4ac4af6856
Rajat Kalsy <crimsonred@users.noreply.github.com>
2018-07-17 15:53:36 +0530

    coverage improvements

--- time_request_test.go
+++ time_request_test.go
@@ -19,3 +19,16 @@ func TestTimeRequestHTTP2(t *testing.T) {
 	assert.Nil(err)
 	assert.Equal(200, s.StatusCode)
 }
+
+func TestNewTimeResponseUnmarshalling(t *testing.T) {
+	assert := assert.New(t)
+	jsonBytes := []byte(`s`)
+
+	_, _, err := newTimeResponse(jsonBytes, fakeResponseState)
+	assert.Equal("pubnub/parsing: Error unmarshalling response: {s}", err.Error())
+
+	opts := &timeOpts{}
+	a, err := opts.buildBody()
+	assert.Nil(err)
+	assert.Equal(a, []byte{})
+}
