79512a160a8ef39410dd7e2c35270acee3dd0129
Serhii Mamontov <parfeon@me.com>
2022-02-11 15:10:30 +0200

    test: adjust test runner script
    Adjust script used as runner.

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -7,6 +7,12 @@ go env
 set -e
 echo "" > coverage.txt
 
+[[ -n "$GOPATH" && ! -d "$GOPATH/src/github.com/pubnub" ]] && \
+  mkdir -p "$GOPATH/src/github.com/pubnub"
+
+# Install 'gocovmerge' module.
+go get -u github.com/wadey/gocovmerge
+
 go test $WITH_MOD -v -coverprofile=functional_tests.out -covermode=atomic -coverpkg=./ ./
 
 go test $WITH_MOD -v -race -coverprofile=utils_tests.out -covermode=atomic -coverpkg=./ ./utils/
@@ -21,6 +27,7 @@ go test $WITH_MOD -v -race -run "TestDestroy\b" -count 20 -coverprofile=deadlock
 go test $WITH_MOD -v -race -run "TestDestroy2\b" -count 20 -coverprofile=deadlock2_tests.out \
 -covermode=atomic -coverpkg=./ ./tests/e2e/
 
+# Send test results for analysis.
 gocovmerge functional_tests.out integration_tests.out utils_tests.out helpers_tests.out deadlock_tests.out deadlock2_tests.out > coverage.txt
 
 rm integration_tests.out functional_tests.out utils_tests.out helpers_tests.out deadlock_tests.out deadlock2_tests.out
