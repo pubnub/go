3cfa3cdff8c65c3aec8839caa775dd599d9ba9d8
Lukasz Klich <lukasz.klich@pubnub.com>
2022-03-18 12:53:27 +0100

    Read pubnub keys for e2e from env

--- tests/e2e/helper.go
+++ tests/e2e/helper.go
@@ -9,6 +9,7 @@ import (
 	"math/rand"
 	"net"
 	"net/http"
+	"os"
 	"time"
 
 	pubnub "github.com/pubnub/go/v7"
@@ -43,14 +44,13 @@ func seedRand() {
 func init() {
 	seedRand()
 	config = pubnub.NewConfig(pubnub.GenerateUUID())
-	config.PublishKey = "pub-c-3ed95c83-12e6-4cda-9d69-c47ba2abb57e"
-	config.SubscribeKey = "sub-c-26a73b0a-c3f2-11e9-8b24-569e8a5c3af3"
+	config.PublishKey = os.Getenv("PUBLISH_KEY")
+	config.SubscribeKey = os.Getenv("SUBSCRIBE_KEY")
 
 	pamConfig = pubnub.NewConfig(pubnub.GenerateUUID())
-	pamConfig.PublishKey = "pub-c-cdea0ef1-c571-4b72-b43f-ff1dc8aa4c5d"
-	pamConfig.SubscribeKey = "sub-c-4757f09c-c3f2-11e9-9d00-8a58a5558306"
-	pamConfig.SecretKey = "sec-c-YTYxNzVjYzctNDY2MS00N2NmLTg2NjYtNGRlNWY1NjMxMDBm"
-
+	pamConfig.PublishKey = os.Getenv("PAM_PUBLISH_KEY")
+	pamConfig.SubscribeKey = os.Getenv("PAM_SUBSCRIBE_KEY")
+	pamConfig.SecretKey = os.Getenv("PAM_SECRET_KEY")
 }
 
 func configCopy() *pubnub.Config {
